b87c1c204fd1030ed815d4aad6576f92238d0d19
cd4d <11580810+cd4d@users.noreply.github.com>
2020-09-26 20:38:51 -0400

    Start routes

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Route, Switch, Link } from "react-router-dom";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import Main from "./Main";
--- frontend/src/app/Ribbon.js
+++ frontend/src/app/Ribbon.js
@@ -1,20 +1,32 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+} from "react-router-dom";
 import Subject from "./Subject";
 import subjects from "../DB/subjects.json";
-import { v4 as uuidv4 } from 'uuid';
-import "./ribbon.css"
+import { v4 as uuidv4 } from "uuid";
+import "./ribbon.css";
 export default function () {
-    const titles = subjects.map(sub => Object.keys(sub))
+  const titles = subjects.map((sub) => Object.keys(sub));
   return (
-      
-    <nav className="ribbon">
-      <ul className="subjects-ribbon">
-        {titles.map((title) => (
-          <li className="subject btn btn-outline-dark mr-3" key={uuidv4()}>
-            <Subject  subjectTitle={title}/>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <>
+      <nav className="ribbon">
+        <ul className="subjects-ribbon">
+          {titles.map((title) => (
+            <li className="subject btn btn-outline-dark mr-3" key={uuidv4()}>
+              <NavLink className="nav-link" to={`/${title}`}>
+                <Subject subjectTitle={title} />
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <Switch>
+        <Route></Route>
+      </Switch>
+    </>
   );
 }
