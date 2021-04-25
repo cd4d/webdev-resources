69fad82d530af6063bf9cdb471da6e22e9464ff9
cd4d <11580810+cd4d@users.noreply.github.com>
2021-04-25 14:30:49 -0400

    Heroku config 2

--- backend/startup/connect-server.js
+++ backend/startup/connect-server.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 
-const port = process.env.PORT || 3000;
-
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 module.exports = function (app) {
   const server = app.listen(port, console.log("Listening on port " + port));
   return server;
